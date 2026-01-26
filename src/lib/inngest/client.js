610ada83735d25a63fc2235d4528b5371ec3d99b
Himanshu singh <himanshusinghrajput1107@gmail.com>
2026-01-26 11:55:15 +0530

    add signing key for inngest

--- src/lib/inngest/client.js
+++ src/lib/inngest/client.js
@@ -3,11 +3,12 @@ import { Inngest } from "inngest";
 // Create a client to send and receive events
 export const inngest = new Inngest({
   id: "DhanAI",
+  signingKey: process.env.INNGEST_SIGNING_KEY,
   name: "DhanAI",
   retryFunction: async (attempt) => ({
     delay: Math.pow(2, attempt) * 1000, // Exponential Backoff
     maxAttempts: 2,
   }),
-})
+});
 
 // This set Ups the Client for the Inngest to work
